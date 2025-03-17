b32ef933743b64e93da3023a8bb9d917895a1865
simonmestabillo <110891346+simonmestabillo@users.noreply.github.com>
2025-03-17 19:52:00 +0800

    Add files via upload

---
+++ joybot-chatbot-geminiapi-main/joybot-chatbot-geminiapi-main/ChatBot Final Project/main.js
@@ -0,0 +1,96 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+
+const API_KEY = "AIzaSyDJD5Rzi01XBd7vGcBNN-ffMoc2Age5fcQ";
+const genAI = new GoogleGenerativeAI(API_KEY);
+const model = genAI.getGenerativeModel({
+      model: "tunedModels/qcenglish-faqs-gillian-ni968gx3lqn0",
+ });
+
+ const generationConfig = {
+    temperature: 0.2,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+
+let messages = {
+    history: [],
+}
+
+async function sendMessage() {
+
+    console.log(messages);
+    const userMessage = document.querySelector(".chat-window input").value;
+    
+    if (userMessage.length) {
+
+        try {
+            document.querySelector(".chat-window input").value = "";
+            document.querySelector(".chat-window .chat").insertAdjacentHTML("beforeend",`
+                <div class="user">
+                    <p>${userMessage}</p>
+                </div>
+            `);
+
+            document.querySelector(".chat-window .chat").insertAdjacentHTML("beforeend",`
+                <div class="loader"></div>
+            `);
+
+            const chat = model.startChat(messages);
+
+            let result = await chat.sendMessageStream(userMessage);
+            
+            document.querySelector(".chat-window .chat").insertAdjacentHTML("beforeend",`
+                <div class="model">
+                    <p></p>
+                </div>
+            `);
+            
+            let modelMessages = '';
+
+            for await (const chunk of result.stream) {
+              const chunkText = chunk.text();
+              modelMessages = document.querySelectorAll(".chat-window .chat div.model");
+              modelMessages[modelMessages.length - 1].querySelector("p").insertAdjacentHTML("beforeend",`
+                ${chunkText}
+            `);
+            }
+
+            messages.history.push({
+                role: "user",
+                parts: [{ text: userMessage }],
+            });
+
+            messages.history.push({
+                role: "model",
+                parts: [{ text: modelMessages[modelMessages.length - 1].querySelector("p").innerHTML }],
+            });
+
+        } catch (error) {
+            document.querySelector(".chat-window .chat").insertAdjacentHTML("beforeend",`
+                <div class="error">
+                    <p>The message could not be sent. Please try again.</p>
+                </div>
+            `);
+        }
+
+        document.querySelector(".chat-window .chat .loader").remove();
+        
+    }
+}
+
+document.querySelector(".chat-window .input-area button")
+.addEventListener("click", ()=>sendMessage());
+
+document.querySelector(".chat-button")
+.addEventListener("click", ()=>{
+    document.querySelector("body").classList.add("chat-open");
+});
+
+document.querySelector(".chat-window button.close")
+.addEventListener("click", ()=>{
+    document.querySelector("body").classList.remove("chat-open");
+});
+
